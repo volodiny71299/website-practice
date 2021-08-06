3dad1ed717e74dec8b5f4d95d847e5448156a576
Yara <58008773+volodiny71299@users.noreply.github.com>
2021-08-06 12:07:13 +1200

    first commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// ini_set('display_errors', 'On'); // change to 'Off' when live
+
+define ("DB_NAME", "volodiny71299_Website_Practice");
+define ("DB_USERNAME", "_volodiny71299");
+define ("DB_PASSWORD", "9aMQsgJznuJSo53Q")
+define ("DB_HOST", "localhost")
+
+?>
