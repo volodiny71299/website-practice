e486f0f019cc05f333b28037e07cf1f8bf8c512c
Yara <58008773+volodiny71299@users.noreply.github.com>
2021-08-11 15:03:52 +1200

    Video 14 finished

---
+++ bottom.php
@@ -0,0 +1,19 @@
+        
+        <div class="box side">
+           
+           <h2>Search Area</h2>
+           
+           <p>This is where the search area goes
+
+            
+        </div> <!-- / side bar -->
+        
+        <div class="box footer">
+            CC Yara Volodin 2021
+        </div> <!-- / footer -->
+                
+        
+    </div> <!-- / wrapper -->
+    
+            
+</body>
--- index.php
+++ index.php
@@ -1,55 +1,4 @@
-<!DOCTYPE HTML>
-
-<html lang="en">
-    
-<?php
-    
-    session_start(); // to allow variable transfer between pages...
-    include("config.php");
-
-    // connect to database...
-    $dbconnect=mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
-    
-    if(mysqli_connect_errno()) {
-        echo "Connection failed:".mysqli_connect_error();
-        exit;
-    }
-    ?>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="description" content="games, apps, ">
-    <meta name="author" content="Yara Volodin">
-    <meta name="keywords" content="games, apps, ratings">
-    
-    <title>Game Database</title>
-
-    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">  
-
-    <!-- for multiple fonts change | to %7c * no spaces*  -->
-    <link href="https://fonts.googleapis.com/css?family=Lato%7cUbuntu" rel="stylesheet">  
-
-    <link rel="stylesheet" href="css/font-awesome.min.css">
-    <link rel="stylesheet" href="css/data_style.css">    <!-- custom style sheet -->
-
-    
-</head>
-
-<body>
-    
-    <p class="message">Eek!  Your browser does not support grid.  Please upgrade your system.</p>
-        
-    <div class="wrapper">
-    
-        <!-- logo / small image goes here -->
-        <div class="box logo">
-            <a href="index.php"><img src="images/logo.png" width="261" height="150" alt="Dice" /></a>
-        </div> <!-- / logo -->
-        
-        <div class="box banner">
-            <h1>Games Database</h1>        
-        </div> <!-- / banner -->
-                       
+<?php include("top.php") ?>
             
         <div class="box main">
             <h2>Welcome</h2>
@@ -62,22 +11,5 @@
 
             
         </div> <!-- / main -->
-        
-        <div class="box side">
-           
-           <h2>Search Area</h2>
-           
-           <p>This is where the search area goes
 
-            
-        </div> <!-- / side bar -->
-        
-        <div class="box footer">
-            CC Yara Volodin 2021
-        </div> <!-- / footer -->
-                
-        
-    </div> <!-- / wrapper -->
-    
-            
-</body>
+<?php include("bottom.php") ?>
